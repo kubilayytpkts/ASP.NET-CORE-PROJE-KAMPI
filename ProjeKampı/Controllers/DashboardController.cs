201e2492f05aa9939820aa51409e38a03240e60c
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-10-22 03:23:04 +0300

    dashboard update-0

---
+++ ProjeKampı/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeKampı.Controllers
+{
+    [AllowAnonymous]
+    public class DashboardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjeKampı/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ProjeKampı.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        BlogManager Manager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var value = Manager.GetListWithCategory();
+            return View(value);
+        }
+    }
+}
