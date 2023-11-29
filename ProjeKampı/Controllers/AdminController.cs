6fc0d12366e71cf1778486ba0201072207b8a131
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-11-29 23:18:10 +0300

    Admin Panel  insert

---
+++ ProjeKampı/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeKampı.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
