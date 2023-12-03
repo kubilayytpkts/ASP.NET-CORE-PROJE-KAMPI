767c7db65459ed7daa93ae7f662338f2372f01ca
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-12-04 00:34:02 +0300

    Admin Panel Update 4.12.2023

---
+++ BusinessLayer/Validations/CategoryValidation.cs
@@ -0,0 +1,24 @@
+﻿using Entity;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+    public class CategoryValidation:AbstractValidator<Category>
+    {
+        public CategoryValidation() 
+        {
+            RuleFor(x=>x.CategoryName).NotEmpty().WithMessage("Kategori adı boş olamaz !");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı 3 karakterden küçük olamaz !");
+            RuleFor(x => x.CategoryName).MaximumLength(15).WithMessage("Kategori adı 15 karakterden büyük olamaz !");
+
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklaması boş olamaz !");
+            RuleFor(x => x.CategoryDescription).MinimumLength(10).WithMessage("Kategori açıklaması 10 karakterden küçük olamaz !");
+            RuleFor(x => x.CategoryDescription).MaximumLength(130).WithMessage("Kategori açıkalaması 130 karakterden büyük olamaz !");
+        }
+    }
+}
---
+++ ProjeKampı/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.Validations;
+using DataAccessLayer.EntityFramework;
+using Entity;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeKampı.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var value = categoryManager.ListAll();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidation validation = new CategoryValidation();
+            ValidationResult validationResult = validation.Validate(category);
+            if(validationResult.IsValid)
+            {
+                category.CategoryStatus = true;
+                categoryManager.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
