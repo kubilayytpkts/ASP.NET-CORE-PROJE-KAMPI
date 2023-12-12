2825ba1dec4cea44bf003eaeeba085a127e26ea3
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-12-13 01:15:10 +0300

    include weatherforcest api

--- ProjeKampı/Areas/Admin/ViewComponents/Widgets/statisticsRowOne.cs
+++ ProjeKampı/Areas/Admin/ViewComponents/Widgets/statisticsRowOne.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading;
+using System.Xml.Linq;
 
 namespace ProjeKampı.Areas.Admin.ViewCompanents.StaticRowOne
 {
@@ -14,6 +15,12 @@ namespace ProjeKampı.Areas.Admin.ViewCompanents.StaticRowOne
         Context context = new Context();
         public IViewComponentResult Invoke()
         {
+            string apiKey = "22bec25ac984b19f72cf1195da069a38";
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q=Turkey&mode=xml&lang=tr&units=metric&appid={apiKey}";
+            XDocument weatherDocument = XDocument.Load(apiUrl);
+            ViewBag.cityWeatherValue = weatherDocument.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+
             ViewBag.blogCount = context.Blogs.Count();
             ViewBag.contactCount = context.Contacts.Count();
             ViewBag.categoryCount = context.Categories.Count();
