d69366f1be685f673f5c140c537240cac80e96a7
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-12-14 23:36:37 +0300

    Update Admin Panel 14.12.23

---
+++ ProjeKampı/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjeKampı.Areas.Admin.Models;
+using System.Collections.Generic;
+
+[Area("Admin")]
+[AllowAnonymous]
+public class ChartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CategoryChart()
+    {
+        List<CategoryClass> list = new List<CategoryClass>();
+        list.Add(new CategoryClass
+        {
+            categoryname = "Teknoloji",
+            categorycount = 10
+        });
+        list.Add(new CategoryClass
+        {
+            categoryname = "Yazılım",
+            categorycount = 14
+        });
+        list.Add(new CategoryClass
+        {
+            categoryname = "Spor",
+            categorycount = 5
+        });
+        list.Add(new CategoryClass
+        {
+            categoryname = "Sinema",
+            categorycount = 2
+        });
+
+        return Json(new { jsonlist = list });
+    }
+}
---
+++ ProjeKampı/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjeKampı.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public string categoryname { get; set; }
+        public int categorycount { get; set; }
+    }
+}
