909144722072bca7759d81639ce69e221a936d49
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-12-24 15:36:45 +0300

    24.12.23

--- ProjeKampı/Areas/Admin/Controllers/WriterController.cs
+++ ProjeKampı/Areas/Admin/Controllers/WriterController.cs
@@ -1,11 +1,14 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using DocumentFormat.OpenXml.Office.CustomUI;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ProjeKampı.Areas.Admin.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ProjeKampı.Areas.Admin.Controllers
 {
@@ -32,29 +35,38 @@ namespace ProjeKampı.Areas.Admin.Controllers
                 });
             }
             
-
-
             var jsonResult = JsonConvert.SerializeObject(writersModels);
             return Json(jsonResult);
         }
 
-        public List<WritersModel> writers = new List<WritersModel>()
+        [HttpGet]
+        public IActionResult AddWriter()
         {
-            new WritersModel()
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterModelForImage writer)
+        {
+            Writer mainWriter = new Writer();
+            if(writer.WriterImage !=null)
             {
-                ID= 1,
-                Name="Şeyma"
-            },
-            new WritersModel()
-            {
-                ID=2,
-                Name="Kubilay",
-            },
-            new WritersModel()
-            {
-                ID=3,
-                Name="Esma"
+                var extension = Path.GetExtension(writer.WriterImage.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/web/images/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                writer.WriterImage.CopyTo(stream);
+                mainWriter.WriterImage = newImageName;
+
             }
-        };
+            mainWriter.WriterName = writer.WriterName;
+            mainWriter.WriterAbout = writer.WriterAbout;
+            mainWriter.WriterPassword = writer.WriterPassword;
+            mainWriter.WriterMail = writer.WriterMail;
+            mainWriter.WriterStatus = true;
+            writerManager.Add(mainWriter);
+            
+            return View();
+        }
     }
 }
--- ProjeKampı/Areas/Admin/Models/WritersModel.cs
+++ ProjeKampı/Areas/Admin/Models/WritersModel.cs
@@ -1,8 +1,20 @@
-﻿namespace ProjeKampı.Areas.Admin.Models
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ProjeKampı.Areas.Admin.Models
 {
     public class WritersModel
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+    }
+    public class WriterModelForImage 
+    {
+        public string WriterName { get; set; }
+        public string WriterAbout { get; set; }
+        public IFormFile WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+        public bool WriterStatus { get; set; }
     }
 }
